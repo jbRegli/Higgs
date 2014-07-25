88a343cc34a0e86dd70adb8c545bcc650c06d5d6
gibonobo <nicolas.gibaud@gmail.com>
2014-07-25 16:11:59 +0800

    Add the concatenated vectors in the return of analyse

--- Analyses/analyse.py
+++ Analyses/analyse.py
@@ -33,12 +33,13 @@ def analyse(train_s, valid_s, method_name, kwargs):
                                                                 train_s[1],
                                                                 train_s[2],
                                                                 valid_s[1],
-                                                                **kwargs)
+                                                            **kwargs)
+
     # Let's convert the four 's' classes in s
     # TODO: Option 4 's' scenario?
     if type(yPredicted_s) == list:
         for i in range(len(yPredicted_s)):
-            for j in range(yPredicted_s[i].shape[0]):
+            for j in range(yPredicted_s[i].shape[0])
                 if yPredicted_s[i][j] >=1:
                     yPredicted_s[i][j] =1
     else:
@@ -46,7 +47,7 @@ def analyse(train_s, valid_s, method_name, kwargs):
                 if yPredicted_s[j] >=1:
                     yPredicted_s[j] =1
 
-    # Let's define the vector of binary probabilities b or s, 0 or 1
+    # Let's define the vector of probabilities of 's'
     if type(yProba_s) == list:
         yProbaBinary_s = []
         for i in range(8):
@@ -59,24 +60,56 @@ def analyse(train_s, valid_s, method_name, kwargs):
         for j in range(yPredicted_s.shape[0]):
             yProbaBinary_s[j] = 1 - yProba_s[j][0]
 
+    # If we work with lists, let's get the concatenated vectors:
+    # Validation Vectors
+    if type(valid_s[2]) == list:
+        yValid_conca = preTreatment.concatenate_vectors(valid_s[2])
+    else:
+        yValid_conca = valid_s[2]
+    # Weights Vectors
+    if type(valid_s[3]) == list:
+        weights_conca = preTreatment.concatenate_vectors(valid_s[3])
+    else:
+        weights_conca = valid_s[3]
+    # Binary Proba Vectors
+    if type(yProbaBinary_s) == list:
+        yProbaBinary_conca = preTreatment.concatenate_vectors(yProbaBinary_s)
+    else:
+        yProbaBinary_conca = yProbaBinary_s
+    # All Proba Vectors
+    if type(yProba_s) == list:
+        yProba_conca = preTreatment.concatenate_vectors(yProba_s)
+    else:
+        yProba_conca = yProba_s
+    # Predicted Vectors
+    if type(yPredicted_s) == list:
+        yPredicted_conca = preTreament.concatenate_vectors(yPredicted_s)
+    else:
+        yPredicted_conca = yPredicted_s
+    
     # Get s and b for each group (s_s, b_s) and the final final_s and
     # final_b:
 
     final_s, final_b, s_s, b_s = submission.get_s_b_8(yPredicted_s, valid_s[2],
                                                   valid_s[3])
+    # Let's get the best global treshold
+    best_treshold_global = postTreatment.best_treshold(yProbaBinary_conca, yValid_conca, weights_conca)
+    yPredicted_conca_treshold = preTreatment.get_yPredicted_treshold(yProbaBinary_conca)
+
+    final_s_treshold, final_b_treshold = submission.get_s_b(yPredicted_conca_treshold, yValid_conca, weights_conca)
+
+
 
     # Balance the s and b
-    if type(valid_s[2]) == list:
-        yValid_conca = preTreatment.concatenate_vectors(valid_s[2])
-    else:
-        yValid_conca = valid_s[2]
-
-    best_treshold_global = postTreatment.best_treshold(y
 
     final_s *= 250000/yValid_conca.shape[0]
     final_b *= 250000/yValid_conca.shape[0]
+    final_s_treshold = 250000/yValid_conca.shape[0]
+    final_b_treshold = 250000/yValid_conca.shape[0]
+
     # AMS final:
     AMS = hbc.AMS(final_s , final_b)
+    AMS_treshold = hbc.AMS(final_s_treshold, treshold)
 
     #AMS by group:
     if type(valid_s[2]) == list:
@@ -108,11 +141,13 @@ def analyse(train_s, valid_s, method_name, kwargs):
         sum_s_s, sum_b_s = submission.get_numerical_score(yPredicted_s,
                                                            valid_s[2])
 
-    d = {'predictor_s':predictor_s, 'yPredicted_s': yPredicted_s,
-         'yProba_s': yProba_s,
+    d = {'predictor_s':predictor_s, 
+         'yPredicted_s': yPredicted_s, 'yPredicted_conca': yPredicted_conca,
+         'yProba_s': yProba_s, 'yProba_conca': yProba_conca,
+         'yProbaBinary_s': yProbaBinary_s, 'yProbaBinary_conca' = yProbaBinary_conca,
          'final_s':final_s, 'final_b':final_b,
          'sum_s':sum_s_s, 'sum_b': sum_b_s,
-         'AMS':AMS, 'AMS_s': AMS_s,
+         'AMS':AMS, 'AMS_s': AMS_s, 'AMS_treshold': AMS_treshold,
          'classif_succ': classif_succ,
          'method': method_name,
          'parameters': kwargs}
