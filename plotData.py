9b5d0be7f52815e174dc9084067c8dde28976e9e
regli <jb_regli@hotmail.fr>
2014-08-07 15:21:28 +0200

    just make sure that everything is up to date

--- plotData.py
+++ plotData.py
@@ -6,7 +6,6 @@ import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-sys.path.append('../')
 import tokenizer
 import preTreatment
 import submission
@@ -14,53 +13,234 @@ import HiggsBosonCompetition_AMSMetric_rev1 as ams
 
 
 def main():
-    ###############
-    ### IMPORT ####
-    ###############
+    train_size = 230000
+    valid_size = 20000
+
+    ##############
+    ### Binary ###
+    ##############
+    split= False
+    normalize = True
+    noise_var = 0.
+
+    print("Binary non splitted ")
+    start = time.clock()
+    train_s, valid_s, test_s = tokenizer.extract_data(split = split,
+                                                      normalize = normalize,
+                                                      noise_variance = 0.,
+                                                      #n_classes = "multiclass",
+                                                      n_classes = "binary",
+                                                      train_size = train_size,
+                                                      train_size2 = 0,
+                                                      valid_size = valid_size)
+
+    # Ploting
+    colors = ['c', 'r']
+    names = ['b', 's']
+
+    for i in range(valid_s[1].shape[1]):
+        for j in range(i):
+            xValue = np.asarray(zip(*valid_s[1]))[i,:]
+            yValue = np.asarray(zip(*valid_s[1]))[j,:]
+            cValue = valid_s[2]
+            axName = valid_s[4]
+
+            b_index = cValue == 0
+            s_index = cValue == 1
+
+            b = plt.scatter(xValue[b_index], yValue[b_index],
+                            c = cValue, color= colors[0])
+            s = plt.scatter(xValue[s_index], yValue[s_index],
+                             c = cValue, color= colors[1])
+
+            plt.legend((b,s), names)
+
+            plt.xlabel(axName[i])
+            plt.ylabel(axName[j])
+
+            title = str(axName[j]) + " vs " + str(axName[i])
+
+            plt.title(title)
+
+            savepath = "./Vizualisation/Binary/All/"
+
+            plt.savefig( savepath + title + ".png", bbox_inches='tight')
+
+
     # Importation parameters:
     split= True
-    normalize = False
+    normalize = True
     noise_var = 0.
-    ratio_train = 0.9
 
-    # Import the training data:
-    print("Extracting the data sets...")
+    print("Binary splited ")
     start = time.clock()
-    train_s, valid_s, test_s = tokenizer.extract_data(split= split,
-                                                      normalize= normalize,
-                                                      noise_variance= noise_var,
-                                                      ratio_train= ratio_train)
-    stop = time.clock()
-    print ("Extraction time: %i s") %(stop-start)
+    train_s, valid_s, test_s = tokenizer.extract_data(split = split,
+                                                      normalize = normalize,
+                                                      noise_variance = 0.,
+                                                      #n_classes = "multiclass",
+                                                      n_classes = "binary",
+                                                      train_size = train_size,
+                                                      train_size2 = 0,
+                                                      valid_size = valid_size)
 
+    # Ploting
+    colors = ['c', 'r']
+    names = ['b', 's']
 
-    print(" ")
-    print(" ")
-
-    ################
-    ### PLOTING ####
-    ################
-
-    # Parameters:
-    sys.argv
-
-    xValid_s = valid_s[1]
-    yValid_s = valid_s[2]
-    nameValid_s = valid_s[4]
-
-    for subset in range(len(valid_s[1])):
-        print "subset %i" %subset
-        # Plot each dimension versus another:
+    for subset in range(len(valid_s[0])):
         for i in range(valid_s[1][subset].shape[1]):
             for j in range(i):
-                plt.scatter(xValid_s[subset][:,i], xValid_s[subset][:,j],
-                        c= yValid_s[subset][:])
+                xValue = np.asarray(zip(*valid_s[1][subset]))[i,:]
+                yValue = np.asarray(zip(*valid_s[1][subset]))[j,:]
+                cValue = valid_s[2][subset]
+                axName = valid_s[4][subset]
 
-                plt.xlabel(nameValid_s[subset][i])
-                plt.ylabel(nameValid_s[subset][j])
-                plt.title("subset %i" %subset)
+                b_index = cValue == 0
+                s2_index = cValue == 1
 
-                plt.show()
+                b = plt.scatter(xValue[b_index], yValue[b_index],
+                                c = cValue, color= colors[0])
+                s = plt.scatter(xValue[s_index], yValue[s_index],
+                                 c = cValue, color= colors[1])
+
+                plt.legend((b,s), names)
+
+                plt.xlabel(axName[i])
+                plt.ylabel(axName[j])
+
+                title = str(axName[j]) + " vs " + str(axName[i])
+
+                plt.title(title)
+
+                savepath = "./Vizualisation/Binary/Subset_" + str(subset) \
+                                + "/"
+
+                plt.savefig( savepath + title + ".png", bbox_inches='tight')
+
+
+
+    ##################
+    ### Multiclass ###
+    ##################
+    split= False
+    normalize = True
+    noise_var = 0.
+
+    print("Mutliclass non splitted ")
+    start = time.clock()
+    train_s, valid_s, test_s = tokenizer.extract_data(split = split,
+                                                      normalize = normalize,
+                                                      noise_variance = 0.,
+                                                      n_classes = "multiclass",
+                                                      #n_classes = "binary",
+                                                      train_size = train_size,
+                                                      train_size2 = 0,
+                                                      valid_size = valid_size)
+
+    # Ploting
+    colors = ['c', 'g', 'y', 'm', 'r']
+    names = ['b', 's1', 's2', 's3', 's4']
+
+    for i in range(valid_s[1].shape[1]):
+        for j in range(i):
+            xValue = np.asarray(zip(*valid_s[1]))[i,:]
+            yValue = np.asarray(zip(*valid_s[1]))[j,:]
+            cValue = valid_s[2]
+            axName = valid_s[4]
+
+            b_index = cValue == 0
+            s1_index = cValue == 1
+            s2_index = cValue == 2
+            s3_index = cValue == 3
+            s4_index = cValue == 4
+
+            b = plt.scatter(xValue[b_index], yValue[b_index],
+                            c = cValue, color= colors[0])
+            s1 = plt.scatter(xValue[s1_index], yValue[s1_index],
+                             c = cValue, color= colors[1])
+            s2 = plt.scatter(xValue[s2_index], yValue[s2_index],
+                             c = cValue, color= colors[2])
+            s3 = plt.scatter(xValue[s3_index], yValue[s3_index],
+                             c = cValue, color= colors[3])
+            s4 = plt.scatter(xValue[s4_index], yValue[s4_index],
+                             c = cValue, color= colors[4])
+
+            plt.legend((b,s1,s2,s3,s4), names)
+
+            plt.xlabel(axName[i])
+            plt.ylabel(axName[j])
+
+            title = str(axName[j]) + " vs " + str(axName[i])
+
+            plt.title(title)
+
+            savepath = "./Vizualisation/MultiClass/All/"
+
+            plt.savefig( savepath + title + ".png", bbox_inches='tight')
+
+
+    # Importation parameters:
+    split= True
+    normalize = True
+    noise_var = 0.
+
+    print("Mutliclass splited ")
+    start = time.clock()
+    train_s, valid_s, test_s = tokenizer.extract_data(split = split,
+                                                      normalize = normalize,
+                                                      noise_variance = 0.,
+                                                      n_classes = "multiclass",
+                                                      #n_classes = "binary",
+                                                      train_size = train_size,
+                                                      train_size2 = 0,
+                                                      valid_size = valid_size)
+
+    # Ploting
+    colors = ['c', 'g', 'y', 'm', 'r']
+    names = ['b', 's1', 's2', 's3', 's4']
+
+    for subset in range(len(valid_s[0])):
+        for i in range(valid_s[1][subset].shape[1]):
+            for j in range(i):
+                xValue = np.asarray(zip(*valid_s[1][subset]))[i,:]
+                yValue = np.asarray(zip(*valid_s[1][subset]))[j,:]
+                cValue = valid_s[2][subset]
+                axName = valid_s[4][subset]
+
+                b_index = cValue == 0
+                s1_index = cValue == 1
+                s2_index = cValue == 2
+                s3_index = cValue == 3
+                s4_index = cValue == 4
+
+                b = plt.scatter(xValue[b_index], yValue[b_index],
+                                c = cValue, color= colors[0])
+                s1 = plt.scatter(xValue[s1_index], yValue[s1_index],
+                                 c = cValue, color= colors[1])
+                s2 = plt.scatter(xValue[s2_index], yValue[s2_index],
+                                 c = cValue, color= colors[2])
+                s3 = plt.scatter(xValue[s3_index], yValue[s3_index],
+                                 c = cValue, color= colors[3])
+                s4 = plt.scatter(xValue[s4_index], yValue[s4_index],
+                                c = cValue, color= colors[4])
+
+                plt.legend((b,s1,s2,s3,s4), names)
+
+                plt.xlabel(axName[i])
+                plt.ylabel(axName[j])
+
+                title = str(axName[j]) + " vs " + str(axName[i])
+
+                plt.title(title)
+
+                savepath = "./Vizualisation/MultiClass/Subset_" + str(subset) \
+                                + "/"
+
+                plt.savefig( savepath + title + ".png", bbox_inches='tight')
+
+
+
+
 
     return 0
 
